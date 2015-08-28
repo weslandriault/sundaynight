9019a8ada0b2622e341733f1603a7f8c0b645c60
Wes <wesley.landriault@gmail.com>
2015-08-28 00:45:41 -0400

    Adding in sliding nav bar at mobile widths.

--- js/sundaynight.js
+++ js/sundaynight.js
@@ -2,12 +2,25 @@
  * Application JS Code For Sunday Night Entertainment (http://http://sundaynight.ca/)
  * Used for stylistic and presentation purposes.
  */
+var navOpen = false;
 
- var mastheadFadeFromBlack = function mastheadFadeFromBlack () {
+var mastheadFadeFromBlack = function mastheadFadeFromBlack () {
   setTimeout(function() {
     $('.navbar-inner').removeClass('black');
     $('#bg-image').removeClass('black');
   }, 50);
- };
+};
 
  mastheadFadeFromBlack();
+
+$('.navbar-toggle').on('click', function () {
+  navOpen = !navOpen;
+
+  if (navOpen) {
+    $('#page').addClass('nav-expanded');
+    $('.icon-bar').addClass('nav-expanded');
+  } else {
+    $('#page').removeClass('nav-expanded');
+    $('.icon-bar').removeClass('nav-expanded');
+  }
+});
